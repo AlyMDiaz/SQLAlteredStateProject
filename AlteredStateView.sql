6e7265a79fdf369678a392cf1273916e5fe4236c
Alyssa Diaz <104540024+AlyMDiaz@users.noreply.github.com>
2023-05-21 23:26:14 -0400

    Add files via upload

--- AlteredStateView.sql
+++ AlteredStateView.sql
@@ -7,10 +7,10 @@ GO
 CREATE VIEW ProjectsBilling
 AS
 	SELECT Info.ProjectID, SUM(Sew.HoursBilled) AS SewingHoursBilled, SUM(Fit.HoursBilled) AS FittingHoursBilled,
-		   CAST(SUM(Fit.HoursBilled) * (SELECT PricePerHour FROM Pricing WHERE WorkCode = 'FIT') AS DECIMAL(6,2)) 
-		   AS FittingPrice, 
 		   CAST(SUM(Sew.HoursBilled) * (SELECT PricePerHour FROM Pricing WHERE WorkCode = 'SEW') AS DECIMAL(6,2))
-		   AS SewingPrice
+		   AS SewingPrice,
+		   CAST(SUM(Fit.HoursBilled) * (SELECT PricePerHour FROM Pricing WHERE WorkCode = 'FIT') AS DECIMAL(6,2)) 
+		   AS FittingPrice
 	FROM ProjectInfo AS Info
 	INNER JOIN ProjectSewingWork AS Sew ON Info.ProjectID = Sew.ProjectID
 	INNER JOIN ProjectFittings AS Fit ON Sew.ProjectID = Fit.ProjectID
@@ -19,4 +19,15 @@ GO
 
 --Test billing view
 SELECT * FROM ProjectsBilling;
+GO
 
+--USE CASE: What has been billed for each project to date for fitting and sewing,
+--  and what is the total price for each project to date, ordered from highest to lowest-priced projects?
+SELECT *, (FittingPrice + SewingPrice) AS TotalPrice, GETDATE() AS DateCalculated
+INTO ProjectsTotalPrices
+FROM ProjectsBilling;
+GO
+
+SELECT * FROM ProjectsTotalPrices
+ORDER BY TotalPrice DESC;
+GO
